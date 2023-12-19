a8b46d18881a44e356f5cdfb167872d928a32561
Jacob Ranson <code@jacobranson.dev>
2023-12-18 21:13:16 -0500

    Force Steam UI scaling

--- overlays/steam/default.nix
+++ overlays/steam/default.nix
@@ -3,7 +3,7 @@
 final: prev: {
   steam = prev.steam.override {
     extraProfile = ''
-      #export STEAM_FORCE_DESKTOPUI_SCALING=2
+      export STEAM_FORCE_DESKTOPUI_SCALING=2
     '';
     extraBwrapArgs = [
       "--chdir ~ --bind ~/Steam ~"
